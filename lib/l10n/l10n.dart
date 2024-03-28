77f645ab48291517b0f3106cf31cd36e9a3c0959
Emmanuele <emagnubcp@gmail.com>
2024-03-28 23:52:34 +0000

    Complete code - forst commit - all working

---
+++ lib/l10n/l10n.dart
@@ -0,0 +1,20 @@
+//   ///
+//  Import LIBRARIES
+import 'package:flutter/material.dart';
+//  Import FILES
+//  PARTS
+//  PROVIDERS
+//  //   ///
+
+//  CLASS
+// lib/l10n/l10n.dart
+class L10n {
+  static final all = [
+    // All the language codes can be found at: https://www.science.co.il/language/Codes.php
+    const Locale('en', 'US'),
+    const Locale('es', 'ES'),
+    const Locale('it', 'IT'),
+    const Locale('zu', 'ZU'),
+// Language code in capital can be omitted
+  ];
+}
